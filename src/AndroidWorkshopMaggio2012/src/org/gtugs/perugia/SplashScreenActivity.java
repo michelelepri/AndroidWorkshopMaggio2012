78bfc01bf61aa0fe77021a0737e3e2f7645701bd
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2012-05-13 11:40:07 +0200

    Resolved enhancement #3 for better SplashScreen

--- src/AndroidWorkshopMaggio2012/src/org/gtugs/perugia/SplashScreenActivity.java
+++ src/AndroidWorkshopMaggio2012/src/org/gtugs/perugia/SplashScreenActivity.java
@@ -1,70 +1,55 @@
-/**
- * 
- */
 package org.gtugs.perugia;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
-/**
- * @author retek http://michelelepri.blogspot.com
- * 
- */
-public class SplashScreenActivity extends Activity
-{
+public class SplashScreenActivity extends Activity {
+	// Flag per determinare se è attivo o meno lo splash
+	private boolean _attivo = true;
 
-   // flag per determinare se Ë attivo o meno lo splash
-   protected boolean _attivo     = true;
+	// Durata dello splash
+	private int _tempoSpash = 5000;
 
-   // durata dello splash
-   protected int     _tempoSpash = 5000;
+	private Thread splashTread = null;
 
-   /*
-    * (non-Javadoc)
-    * 
-    * @see android.app.Activity#onCreate(android.os.Bundle)
-    */
-   @Override
-   protected void onCreate(Bundle savedInstanceState)
-   {
-      super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_screen);
 
-      setContentView(R.layout.splash_screen);
+		// Thread per far vedere la spash screen
+		splashTread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					int tempoPassato = 0;
+					while (_attivo && (tempoPassato < _tempoSpash)) {
+						sleep(100);
+						tempoPassato += 100;
+					}
+				} catch (InterruptedException e) {
+					Log.d("Splash", e.getMessage());
+				} finally {
+					finish();
+					// Avvia l'Activity principale
+					Intent i = new Intent(SplashScreenActivity.this,
+							MainActivity.class);
+					startActivity(i);
+				}
+			}
+		};
 
-      // thread per far vedere la spash screen
-      Thread splashTread = new Thread() {
+		splashTread.start();
+	}
 
-         @Override
-         public void run()
-         {
-            try
-            {
-               int tempoPassato = 0;
-               while (_attivo && (tempoPassato < _tempoSpash))
-               {
-                  sleep(100);
-                  if (_attivo)
-                  {
-                     tempoPassato += 100;
-                  }
-               }
-            }
-            catch (InterruptedException e)
-            {
-               // non facciamo nulla, andrebbe almeno loggato qualcosa comunque
-
-            }
-            finally
-            {
-               finish();
-               
-               // lancio l'activity
-               Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
-               startActivity(i);
-            }
-         }
-      };
-      splashTread.start();
-   }
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			_attivo = false;
+		}
+		return super.onTouchEvent(event);
+	}
 }
