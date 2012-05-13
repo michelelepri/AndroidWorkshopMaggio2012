7d2ec10dfa4d25420e39ced4112926893096a4b5
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2012-05-13 14:30:35 +0200

    Resolved Enhancement #3 adding WebView error handling and the visualization of progress bar

--- src/AndroidWorkshopMaggio2012/src/org/gtugs/perugia/MainActivity.java
+++ src/AndroidWorkshopMaggio2012/src/org/gtugs/perugia/MainActivity.java
@@ -6,35 +6,65 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
+import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private WebView _webView;
 	private EditText _url;
+	private MainActivity _activity = null;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		_webView = (WebView) findViewById(R.id.webview);
-		
-		// Viene abilitato il javascript
-		_webView.getSettings().setJavaScriptEnabled(true);
+		_activity = this;
 
-		/*
-		 * Override del metodo UrlLoading per far aprire i link selezionati
-		 * nella WebView
-		 */
+		// Richiede la feature per mostrare la barra di progressione
+		requestWindowFeature(Window.FEATURE_PROGRESS);
+
+		setContentView(R.layout.main);
+		setProgressBarVisibility(true);
+
+		_webView = (WebView) findViewById(R.id.webview);
 		_webView.setWebViewClient(new WebViewClient() {
+			// Carica i link nell'app anziché nel browser standard
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {
 				view.loadUrl(url);
 				return true;
 			}
+
+			// Notifica gli errori di caricamento tramite TOAST
+			public void onReceivedError(WebView view, int errorCode,
+					String description, String failingUrl) {
+				Toast.makeText(_activity, "Oh no! " + description,
+						Toast.LENGTH_SHORT).show();
+			}
 		});
+
+		_webView.setWebChromeClient(new WebChromeClient() {
+			// Barra del titolo come progressione nel caricamento della pagina
+			public void onProgressChanged(WebView view, int progress) {
+				_activity.setProgress(progress * 100);
+			}
+
+			// Gestisce gli alert Javascript
+			public boolean onJsAlert(WebView view, String url, String message,
+					final android.webkit.JsResult result) {
+				Toast.makeText(_activity.getApplicationContext(), message, 3000)
+						.show();
+				result.confirm();
+				return true;
+			};
+		});
+
+		// Viene abilitato il javascript
+		_webView.getSettings().setJavaScriptEnabled(true);
 
 		_url = (EditText) findViewById(R.id.url);
 		_url.setOnKeyListener(new OnKeyListener() {
@@ -67,7 +97,7 @@ public class MainActivity extends Activity {
 		// Ricarico l'url
 		_webView.loadUrl(_url.getText().toString());
 	}
-	
+
 	/*
 	 * Metodi per gestire la WebView
 	 */
